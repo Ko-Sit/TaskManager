3e28362d6da7caf788504d3ed888d214881f2735
Sitkin, Konstantin <ksitkin@qulix.com>
2017-06-27 12:20:08 +0300

    Created constants file for jsp paths

---
+++ com.qulix.sitkinke.trainingtask.server/src/main/java/com/qulix/sitkinke/trainingtask/constants/PathConfigs.java
@@ -0,0 +1,24 @@
+package com.qulix.sitkinke.trainingtask.constants;
+
+/**
+ *
+ * Created by upsit on 27.06.2017.
+ */
+public class PathConfigs {
+    public static final String INDEX_PAGE = "path.page.index";
+    public static final String MAIN_PAGE = "path.page.main";
+    public static final String SHOW_EMPLOYEES_PAGE = "path.page.showemployees";
+    public static final String ADD_EMPLOYEE_PAGE = "path.page.addemployee";
+    public static final String MODIFY_EMPLOYEE_PAGE = "path.page.modifyemployee";
+    public static final String SHOW_TASKS_PAGE = "path.page.showtasks";
+    public static final String ADD_TASK_PAGE = "path.page.addtask";
+    public static final String ADD_TEMP_TASK_PAGE = "path.page.addtemptask";
+    public static final String MODIFY_TASK_PAGE = "path.page.modifytask";
+    public static final String MODIFY_TEMP_TASK_PAGE = "path.page.modifytemptask";
+    public static final String ADD_TASK_FROM_PROJECT_PAGE = "path.page.addtaskfromproject";
+    public static final String MODIFY_TASK_FROM_PROJECT_PAGE = "path.page.modifytaskfromproject";
+    public static final String SHOW_PROJECTS_PAGE = "path.page.showprojects";
+    public static final String ADD_PROJECT_PAGE = "path.page.addproject";
+    public static final String MODIFY_PROJECT_PAGE = "path.page.modifyproject";
+
+}
